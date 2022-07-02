52ed13a235bff28734ff4f899c79a96cd8d14577
Pol Gubau <gubaupol@gmail.com>
2022-07-02 09:28:28 +0200

    first push

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "plugin:react/recommended",
+        "standard"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    "rules": {
+        "indent": ["warn",2],
+        "linebreak-style": ["warn", "unix"],
+        "eqeqeq": ["error"],
+        "semi": ["off", "never"],
+        "quotes": ["error", "double"],
+        "no-console": ["warn"],
+        "no-unused-vars": ["warn"],
+        
+    }
+}

e780f72dd1b54f71a6e1fcf74071b94290186be7
Pol Gubau <gubaupol@gmail.com>
2022-07-10 15:54:05 +0200

    users API call

--- src/pages/api/users.js
+++ src/pages/api/users.js
@@ -1,41 +1,107 @@
-export default function (req, res) {
-  const allUsers =  [
-    {
-      id: '0',
-      userName: 'pol',
-      email: 'gubaupol@gmail.com',
-      avatar: 'https://api.multiavatar.com/Pol.png',
-      password: '123456',
-      since: '2020-01-01',
-      collections: ['0', '1', '3'],
-    },
-    {
-      id: '1',
-      userName: 'anna',
-      email: 'annacampsromero@gmail.com',
-      avatar: 'https://api.multiavatar.com/Anna.png',
-      password: '123456',
-      since: '2020-01-01',
-      collections: ['0', '3'],
-    },
-    {
-      id: '2',
-      userName: 'marcos',
-      email: 'marcos@gmail.com',
-      avatar: 'https://api.multiavatar.com/marcos.png',
-      password: '123456',
-      since: '2020-01-01',
-      collections: ['0', '3'],
-    },
-    {
-      id: '3',
-      userName: 'marc',
-      email: 'marc@gmail.com',
-      avatar: 'https://api.multiavatar.com/marc.png',
-      password: '123456',
-      since: '2020-01-01',
-      collections: ['0', '3'],
-    },
-  ]
-  res.status(200).json(allUsers)
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const { connectToDatabase } = require('../../../libs/mongodb')
+const ObjectId = require('mongodb').ObjectId
+
+export default async function handler(req, res) {
+  // switch the methods
+  switch (req.method) {
+    case 'GET': {
+      return getUsers(req, res)
+    }
+
+    case 'POST': {
+      return addPost(req, res)
+    }
+
+    case 'PUT': {
+      return updatePost(req, res)
+    }
+
+    case 'DELETE': {
+      return deletePost(req, res)
+    }
+  }
+}
+
+// Getting all users.
+async function getUsers(req, res) {
+  try {
+    const { db } = await connectToDatabase()
+    const users = await db
+      .collection('users')
+      .find({})
+      .sort({ published: -1 })
+      .toArray()
+    return res.json({
+      message: JSON.parse(JSON.stringify(users)),
+      success: true,
+    })
+  } catch (error) {
+    return res.json({
+      message: new Error(error).message,
+      success: false,
+    })
+  }
+}
+
+// Adding a new post
+async function addPost(req, res) {
+  try {
+    const { db } = await connectToDatabase()
+    await db.collection('users').insertOne(JSON.parse(req.body))
+    return res.json({
+      message: 'Post added successfully',
+      success: true,
+    })
+  } catch (error) {
+    return res.json({
+      message: new Error(error).message,
+      success: false,
+    })
+  }
+}
+
+// Updating a post
+async function updatePost(req, res) {
+  try {
+    const { db } = await connectToDatabase()
+
+    await db.collection('users').updateOne(
+      {
+        _id: new ObjectId(req.body),
+      },
+      { $set: { published: true } }
+    )
+
+    return res.json({
+      message: 'Post updated successfully',
+      success: true,
+    })
+  } catch (error) {
+    return res.json({
+      message: new Error(error).message,
+      success: false,
+    })
+  }
+}
+
+// deleting a post
+async function deletePost(req, res) {
+  try {
+    const { db } = await connectToDatabase()
+
+    await db.collection('users').deleteOne({
+      _id: new ObjectId(req.body),
+    })
+
+    return res.json({
+      message: 'Post deleted successfully',
+      success: true,
+    })
+  } catch (error) {
+    return res.json({
+      message: new Error(error).message,
+      success: false,
+    })
+  }
 }
